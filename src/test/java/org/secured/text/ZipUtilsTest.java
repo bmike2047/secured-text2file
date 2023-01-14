edadb84b29f6cccc193566e98c8f8a17f07727f7
bmike2047 <lonebot2003@gmail.com>
2023-01-14 00:58:02 +0000

    Added tests

---
+++ src/test/java/org/secured/text/ZipUtilsTest.java
@@ -0,0 +1,42 @@
+package org.secured.text;
+
+import com.secured.text.ZipUtils;
+import net.lingala.zip4j.model.enums.AesKeyStrength;
+import net.lingala.zip4j.model.enums.CompressionMethod;
+import net.lingala.zip4j.model.enums.EncryptionMethod;
+import org.apache.commons.lang3.RandomStringUtils;
+import org.junit.jupiter.api.Test;
+
+import java.io.File;
+import java.nio.charset.StandardCharsets;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.logging.Logger;
+
+public class ZipUtilsTest {
+    public static final Logger L = Logger.getLogger("org.secured.text");
+    @Test
+    void testEncryptionBiDirectional() throws Exception {
+        Path path = Paths.get("test.zip");
+        File file = new File(path.toUri());
+        String password = "some-random-pass";
+        String plainText = "This is me encrypted";
+
+        ZipUtils.writeEncryptedFile(file,plainText,
+                RandomStringUtils.randomAlphanumeric(8),
+                password.toCharArray(),
+                CompressionMethod.DEFLATE,true,
+                EncryptionMethod.AES,
+                AesKeyStrength.KEY_STRENGTH_256);
+
+        byte[] fileContent = Files.readAllBytes(path);
+        L.info("Encrypted file content: " + new String( fileContent, StandardCharsets.UTF_8));
+
+        String decryptedText = ZipUtils.readEncryptedFile(file, password.toCharArray());
+
+        assert(decryptedText).equals(plainText);
+        L.info("Temp file deleted: " + file.delete());
+
+    }
+}
