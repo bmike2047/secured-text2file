34708141444c81e3749517d809c8b7fc14ddd82f
mike <lonebot2003@yahoo.com>
2024-07-23 13:26:16 +0100

    Cleaned up project name and updated linux binaries

--- src/main/java/secured/text/SecuredTextApplication.java
+++ src/main/java/secured/text/SecuredTextApplication.java
@@ -12,7 +12,7 @@ public class SecuredTextApplication extends Application {
     public void start(Stage stage) throws IOException {
         FXMLLoader fxmlLoader = new FXMLLoader(SecuredTextApplication.class.getResource("secured-view.fxml"));
         Scene scene = new Scene(fxmlLoader.load(), 700, 600);
-        stage.setTitle("Secured Text 2 File v2");
+        stage.setTitle("Secured Text 2 File");
         stage.setScene(scene);
         stage.show();
     }
